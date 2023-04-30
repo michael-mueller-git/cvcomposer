1a1e0afca3e714a115bf74224cda68f4dd3ba556
arch <arch@local>
2023-04-30 20:07:08 +0200

    add flake template

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      cvcomposer = (with pkgs; stdenv.mkDerivation {
+          pname = "cvcomposer";
+          version = "1.3.1";
+          src = fetchgit {
+            url = "https://github.com/michael-mueller-git/cvcomposer.git";
+            rev = "774388876aedc4b00afaebae6a8477710599d7d5";
+            fetchSubmodules = true;
+          };
+          nativeBuildInputs = [
+            clang
+            cmake
+            qt6.full
+            opencv
+          ];
+          buildPhase = "make -j $NIX_BUILD_CORES";
+          installPhase = ''
+            mkdir -p $out/bin
+            mv $TMP/cvcomposer $out/bin
+          '';
+        }
+      );
+    in rec {
+      defaultApp = flake-utils.lib.mkApp {
+        drv = defaultPackage;
+      };
+      defaultPackage = cvcomposer;
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          cvcomposer
+          qt6.full
+          opencv
+        ];
+      };
+    }
+  );
+}
